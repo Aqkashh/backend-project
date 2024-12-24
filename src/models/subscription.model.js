3a5f4f2dff7ce8ad058063ea70130e070f601962
Aqkashh <122206736+Aqkashh@users.noreply.github.com>
2024-12-24 14:45:30 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,20 @@
+import mongoose,{Schema} from "mongoose";
+
+
+const subscriptionSchema = new Schema ({})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const Subscription = mongoose.model("ubscriptionSchema")
