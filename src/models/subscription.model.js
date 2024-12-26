795c55b1f0bc647fc768d52e275fd6c13d8c4f6d
Aqkashh <122206736+Aqkashh@users.noreply.github.com>
2024-12-26 13:56:49 +0530

    Update subscription.model.js

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,20 +1,16 @@
 import mongoose,{Schema} from "mongoose";
 
 
-const subscriptionSchema = new Schema ({})
+const subscriptionSchema = new Schema ({
+    subscriber:{
+        type :Schema.Types.ObjectId,
+        ref: "User"
+    },
+    channel:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export const Subscription = mongoose.model("ubscriptionSchema")
+export const Subscription = mongoose.model("Subscription",subscriptionSchema)
